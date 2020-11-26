4ee10f5874daa35ddc1bdeed0c0255d51ea7355f
yangh <yangh@DESKTOP-0U4BS9N>
2020-11-26 22:49:54 +0900

    숫자야구 까지 함.

---
+++ 4. 반응속도체크/ResponseCheck.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+     <div id="screen">{{ message }}</div>
+     <div>
+         <div>평균시간 : {{ }}</div>
+         <button v-on:click="onReset">리셋</button>
+     </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            result: [],
+            state: 'waiting',
+            message: '클릭해서 시작하세요.',
+        }
+    },
+    methods: {
+        onReset() {
+
+        }
+    },
+};
+</script>
+
+<style>
+
+</style>
